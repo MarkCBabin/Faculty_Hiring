9e67bc5172c7210f6324fadfc99fb2c3825985cc
Mark Babin <markcbabin@gmail.com>
2019-03-20 19:58:06 -0700

    Update Faculty_Hiring.py
    Finalized version!

--- Faculty_Hiring.py
+++ Faculty_Hiring.py
@@ -39,13 +39,13 @@ f1.write('Graduate Student Evaluation of ' + str(sys.argv[2])+' ' + str(sys.argv
 # into the sections that it will print out 
 cols = pd.read_csv(inFile,header=0,usecols=[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18])
 
-for i in range (5):
-	for j in range (16):
+for i in range (6):
+	for j in range (17):
 		test = isinstance(cols.iloc[i,j], basestring)
 		if test is False:
 			test1 = math.isnan(cols.iloc[i,j])
 			if test1 is True:
-				cols.iloc[i,j] = str()
+				cols.iloc[i,j] = 'N/A'
 # this set of for/if loops cleans up the data - as pandas reads things
 # a blank space becomes a NaN, so this converts all NaNs back into blank spaces
 			
@@ -71,8 +71,45 @@ expectations = str(cols.iloc[0,8]) + ' ' + str(cols.iloc[1,8]) +  ' ' + str(cols
 
 ########################################################################################################################
 
-f1.write(str(cols.iloc[1,9]))
+f1.write('Potential contributions to research\n' + research + '\n')
+f1.write('Potential contributions as a teacher or lecturer\n' + teaching + '\n')
+f1.write('Potential contributions to mentoring\n' + mentoring + '\n')
+f1.write('Potential contributions to diversity, equity, and inclusion\n' + DEI + '\n')
+f1.write('Treated student evaluators with dignity and respect\n' + respect + '\n')
+f1.write('Has a clear plan to foster a positive work environment in their lab\n' + environment + '\n')
+f1.write('Can articulate strategy for handling conflicts (esp. relating to assigning authorship)\n' + conflict + '\n')
+f1.write('Has a concrete plan for gathering feedback from students about lab management and graduate student mentorship\n' + feedback + '\n')
+f1.write('Has a clear plan for articulating expectations to graduate students\n' + expectations + '\n')
 
+f1.write('Irrespective of your field, how exciting do you find their research to be?\n')
+for i in range (6):
+	f1.write('- ' + str(cols.iloc[i,9])+'\n')
 
-f1.write('-')
+f1.write('Would they be a good recruiter for bringing new students to the department?\n')
+for i in range (6):
+	f1.write('- ' + str(cols.iloc[i,10])+'\n')
+
+f1.write('For statements where you selected a fair or poor, does the faculty candidate have specific plans to improve these areas? If so, please describe them.\n')
+for i in range (6):
+	f1.write('- ' + str(cols.iloc[i,11])+'\n')
+
+f1.write('Please describe any concrete examples that the faculty candidate gave to demonstrate that he/she has prepared himself/herself to mentor graduate students and/or manage a research group.\n')
+for i in range (6):
+	f1.write('- ' + str(cols.iloc[i,12])+'\n')
+
+f1.write('What impresses you the most about this candidate?\n')
+for i in range (6):
+	f1.write('- ' + str(cols.iloc[i,13])+'\n')
+
+f1.write('What concerns you the most about this candidate?\n')
+for i in range (6):
+	f1.write('- ' + str(cols.iloc[i,14])+'\n')
+
+f1.write('Is this a candidate that you think would be a good addition to our faculty? Why?\n')
+for i in range (6):
+	f1.write('- ' + str(cols.iloc[i,15])+'\n')
+
+f1.write('Is there any other information that you would like to provide to the hiring committee concerning this faculty candidate?\n')
+for i in range (6):
+	f1.write('- ' + str(cols.iloc[i,16])+'\n')
 
